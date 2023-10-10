331ddb80ea1ef301bf580a5979fd0c3ba500d563
Kulbhushan <karnkulbhushan@gmail.com>
2023-10-10 12:56:17 +0530

    New commit

--- Bitwise/Program.cs
+++ Bitwise/Program.cs
@@ -10,8 +10,45 @@ namespace Bitwise
     {
         static void Main()
         {
+
+            string userInput;
+
+            userInput = Console.ReadLine();
+            int a = Convert.ToInt32(userInput);
+            Console.WriteLine(a);
+
+            userInput = Console.ReadLine();
+            long b = Convert.ToInt64(userInput);
+            Console.WriteLine(b);
+
+            userInput = Console.ReadLine();
+            char c = Convert.ToChar(userInput);
+            Console.WriteLine(c);
+
+            userInput = Console.ReadLine();
+            float d = Convert.ToSingle(userInput);
+            Console.WriteLine(d);
+
+            userInput = Console.ReadLine();
+            double e = Convert.ToDouble(userInput);
+            Console.WriteLine(e);
+
+
+
             int num1 = 10;
             int num2 = 6;
+
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+
             int resultAnd = num1 & num2; 
             Console.WriteLine($"Bitwise AND: {resultAnd}");
 
@@ -22,7 +59,7 @@ namespace Bitwise
             Console.WriteLine($"Bitwise XOR: {resultXor}");
 
             int num3 = 5;  
-            int resultNot = ~num3; 
+            int resultNot = ~ num3; 
             Console.WriteLine($"Bitwise NOT: {resultNot}");
 
             int num4 = 8; 
